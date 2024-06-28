c014fe1995c5005c783e26d53be5be23cd9f9c07
Samuel R Borges <sammy_rb@hotmail.com>
2024-06-28 12:16:12 -0300

    processing data

--- user-request-sb/src/main/java/com/devsuperior/user_request_sb/step/FetchUserDataAndStoreDBStepConfig.java
+++ user-request-sb/src/main/java/com/devsuperior/user_request_sb/step/FetchUserDataAndStoreDBStepConfig.java
@@ -2,9 +2,11 @@ package com.devsuperior.user_request_sb.step;
 
 
 import com.devsuperior.user_request_sb.dto.UserDTO;
+import com.devsuperior.user_request_sb.entities.User;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,11 +27,13 @@ public class FetchUserDataAndStoreDBStepConfig {
 
     @Bean
     public Step fetchUserDataAndStoreDBStep(ItemReader<UserDTO> fetchUserDataReader,
-                                            ItemWriter<UserDTO> insertUserDataDBWriter,
+                                            ItemProcessor<UserDTO, User> selectFieldsUserDataProcessor,
+                                            ItemWriter<User> insertUserDataDBWriter,
                                             JobRepository jobRepository) {
         return new StepBuilder("fetchUserDataAndStoreDBStep", jobRepository)
-                .<UserDTO, UserDTO>chunk(chunkSize, platformTransactionManager)
+                .<UserDTO, User>chunk(chunkSize, platformTransactionManager)
                 .reader(fetchUserDataReader)
+                .processor(selectFieldsUserDataProcessor)
                 .writer(insertUserDataDBWriter)
                 .build();
     }
